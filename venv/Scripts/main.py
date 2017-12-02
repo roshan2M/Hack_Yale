c6f9100e3c56bfb10631ce7d73c504d0d0ef32ec
Roshan Munjal <roshan.munjal@hotmail.com>
2017-12-02 02:32:50 -0500

    added csv support

--- venv/Scripts/main.py
+++ venv/Scripts/main.py
@@ -1,1 +1,31 @@
-print ("Hello World")
+import csv
+
+assets = []
+profits = []
+liabilities = []
+assets_st = []
+assets_lt = []
+equity = []
+sales = []
+employees = []
+
+with open('report1.csv') as f:
+    rows = csv.reader(f)
+    for row in rows:
+        assets.append(row[3])
+        profits.append(row[4])
+        liabilities.append(row[8])
+        assets_st.append(row[9])
+        assets_lt.append(row[10])
+        equity.append(row[11])
+        sales.append(row[12])
+        employees.append(row[13])
+
+assets = assets[1:]
+profits = profits[1:]
+liabilities = liabilities[1:]
+assets_st = assets_st[1:]
+assets_lt = assets_lt[1:]
+equity = equity[1:]
+sales = sales[1:]
+employees = employees[1:]
