5ed9c96d49a6fd730ac2057994ea96e3f18213f8
Matthew Chaing <matthew.chiang.myc@gmail.com>
2017-12-03 05:01:37 -0500

    added distribution

---
+++ venv/Scripts/distribution.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def computeMean(set):
+    "inputs a panda DataFrame of any length (14 columns) and outputs a numpy array with 7 outputs"
+    # Assets, Profits, Liabilities, Short term assets, Long term assets, equity, and sales
+
+    mean = []
+    mean.append(set.assets.mean())
+    mean.append(set.profits.mean())
+    mean.append(set.liabilities.mean())
+    mean.append(set.assets_short_term.mean())
+    mean.append(set.assets_long_term.mean())
+    mean.append(set.equity.mean())
+    mean.append(set.sales.mean())
+
+    return mean
+
+def computeStd(set):
+    "same parameters and return as computeMean but std dev instead of mean"
+    std = []
+    std.append(set.assets.std())
+    std.append(set.profits.std())
+    std.append(set.liabilities.std())
+    std.append(set.assets_short_term.std())
+    std.append(set.assets_long_term.std())
+    std.append(set.equity.std())
+    std.append(set.sales.std())
+
+    return std
+
+def computeMin(set):
+    "same parameters and return as computeMean but min dev instead of mean"
+    min = []
+    min.append(set.assets.min())
+    min.append(set.profits.min())
+    min.append(set.liabilities.min())
+    min.append(set.assets_short_term.min())
+    min.append(set.assets_long_term.min())
+    min.append(set.equity.min())
+    min.append(set.sales.min())
+
+    return min
+
+def computeMax(set):
+    "same parameters and return as computeMean but max dev instead of mean"
+    max = []
+    max.append(set.assets.max())
+    max.append(set.profits.max())
+    max.append(set.liabilities.max())
+    max.append(set.assets_short_term.max())
+    max.append(set.assets_long_term.max())
+    max.append(set.equity.max())
+    max.append(set.sales.max())
+
+    return max
+
+def stdInterval(set,interval):
+    "accepts a panda dataframe, the amout of intervals the data will be split into and the specific category"
+    #returns a 2d numpy array where each row is a different data set ie. assets, profits etc.
+    #each column is a different interval
+
+    """sorted_assets = set.assets.sort_values()
+    sorted_profits = set.profits.sort_values()
+    sorted_liabilities = set.liabilities.sort_values()
+    sorted_assets_short_term = set.assets_short_term.sort_values()
+    sorted_assets_long_term = set.assets_long_term.sort_values()
+    sorted_equity = set.equity.sort_values()
+    sorted_sales = set.sales.sort_values()"""
+
+    min = computeMin(set)
+    max = computeMax(set)
+
+    scaled = set.copy()
+
+    #scaling all values from 0-100
+    for i in range(len(set.assets)):
+        scaled.iat[i,3] = set.iat[i,3]*100/max[0]
+        scaled.iat[i,4] = set.iat[i,4]*100/max[1]
+        scaled.iat[i,8] = set.iat[i,8]*100/max[2]
+        scaled.iat[i,9] = set.iat[i,9]*100/max[3]
+        scaled.iat[i,10] = set.iat[i,10]*100/max[4]
+        scaled.iat[i,11] = set.iat[i,11]*100/max[5]
+        scaled.iat[i,12] = set.iat[i,12]*100/max[6]
+
+    #1st interval
+    interWidth =(100/interval)
+    hist = np.zeros((7,interval),dtype=np.int)
+
+    for j in range(len(set.assets)):
+        for i in range(1,interval+1):
+            if(scaled.iat[j,3] <i*interWidth and scaled.iat[j,3] > (i-1)*interWidth):
+                hist[0,i-1] += 1
+            if(scaled.iat[j,4] <i*interWidth and scaled.iat[j,4] > (i-1)*interWidth):
+                hist[1,i-1] += 1
+            if(scaled.iat[j,8] < i*interWidth and scaled.iat[j,8] > (i-1)*interWidth):
+                hist[2,i-1] += 1
+            if(scaled.iat[j,9] < i*interWidth and scaled.iat[j,9] > (i-1)*interWidth):
+                hist[3,i-1] += 1
+            if(scaled.iat[j,10] < i*interWidth and scaled.iat[j,10] > (i-1)*interWidth):
+                hist[4,i-1] += 1
+            if(scaled.iat[j,11] < i*interWidth and scaled.iat[j,11] > (i-1)*interWidth):
+                hist[5,i-1] += 1
+            if(scaled.iat[j,12] < i*interWidth and scaled.iat[j,12] > (i-1)*interWidth):
+                hist[6,i-1] += 1
+
+
+    scaled.drop('rating',axis = 1,inplace = True)
+    scaled.drop('id',axis = 1,inplace = True)
+    scaled.drop('reporting_date',axis = 1,inplace = True)
+    scaled.drop('pd',axis = 1,inplace = True)
+    scaled.drop('maturity_date',axis = 1,inplace = True)
+    scaled.drop('state',axis = 1,inplace = True)
+    scaled.drop('employees',axis = 1,inplace = True)
+
+    #print(scaled)
+    print("hdf")
+    #plt.figure();
+    #scaled.plot.hist(bins = 20)
+    #plt.show()
+    return (hist)
+
+
+    #frames = [sorted_assets, sorted_profits, sorted_liabilities, sorted_assets_short_term,sorted_assets_long_term,sorted_equity,sorted_sales]
+    #sorted_total = pd.concat(frames,axis = 1)
+
+    #print(sorted_total)
+
+def fillAnsArr(set1,set2,alpha,set,setType):
+    "fills output matrix with correct and incorrect data in dictionaries all in a 2d array"
+    #setType is an int determining what type of set we are analyzing
+    #returns a 1d array of dictionaries (2)
+    correct = {}
+    incorrect = {}
+    print(set)
+    for i in range(len(set[0])): # how many intervals
+        avg = (set1[0][i] + set2[0][i])/2
+        if(set[setType][i] > avg*(1-alpha) and set[setType][i]<avg*(1+alpha)):
+            correct[i] = set[setType][i]
+        else:
+            incorrect[i] = set[setType][i]
+    return [correct,incorrect]
+
+def findAnomalies (set1,set2,seta,alpha):
+    "finds anomalies in a set using tolerance alpha(alpha must be from 0-1)"
+    #set1 2 3 are 2d numpy arrays with 7 rows and the same number of columns (each column is a interval)
+
+    answer = []
+    for i in range(0,7): #going hrough the different columns
+        answer.append(fillAnsArr(set1,set2,alpha,seta,i))
+
+    return answer
--- venv/Scripts/main.py
+++ venv/Scripts/main.py
@@ -1,8 +1,21 @@
 import csv
 import os
-import AnomalyDetection
+import distribution
 import pandas as pd
 import numpy as np
+
+def pandaStrtoFlt(temp):
+    "converts the applicable string fields to integers"
+    temp.assets = temp.assets.astype(float)
+    temp.profits = temp.profits.astype(float)
+    temp.pd = temp.pd.astype(float)
+    temp.liabilities = temp.liabilities.astype(float)
+    temp.assets_short_term = temp.assets_short_term.astype(float)
+    temp.assets_long_term = temp.assets_long_term.astype(float)
+    temp.equity = temp.equity.astype(float)
+    temp.sales = temp.sales.astype(float)
+    temp.employees = temp.employees.astype(int)
+    return temp
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 parent_directory1 = os.path.split(my_path)[0]
@@ -12,132 +25,29 @@ path2 = os.path.join(parent_directory2, 'Data', 'report2.csv')
 path3 = os.path.join(parent_directory2, 'Data', 'report3.csv')
 path4 = os.path.join(parent_directory2, 'Data', 'report4.csv')
 
-Y1 = pd.read_csv(path1)#,dtype = np.float64)
+Y1 = pd.read_csv(path1)
 Y2 = pd.read_csv(path2)
 Y3 = pd.read_csv(path3)
 Y4 = pd.read_csv(path4)
 
-frames = [Y1,Y2,Y3,Y4]
-YT = pd.concat(frames)
+Y1 = pandaStrtoFlt(Y1)
+Y2 = pandaStrtoFlt(Y2)
+Y3 = pandaStrtoFlt(Y3)
+Y4 = pandaStrtoFlt(Y4)
 
-#print(assets)
-X = []
-for i in range(len(YT)):
-    X.append(i)
+trainingFrames = [Y1, Y2]
+YT = pd.concat(trainingFrames)
 
+hist1 = distribution.stdInterval(Y1,4)
+hist2 = distribution.stdInterval(Y2,4)
+hist3 = distribution.stdInterval(Y3,4)
+hist4 = distribution.stdInterval(Y4,4)
 
-Xp = pd.DataFrame(X,columns = ['Index'])
-#Yp = pd.DataFrame(assets,columns = ['Assets'])
-YT.assets = YT.assets.astype(float)
-YT.profits = YT.profits.astype(float)
-YT.pd = YT.pd.astype(float)
-YT.liabilities = YT.liabilities.astype(float)
-YT.assets_short_term = YT.assets_short_term.astype(float)
-YT.assets_long_term = YT.assets_long_term.astype(float)
-YT.equity = YT.equity.astype(float)
-YT.sales = YT.sales.astype(float)
-YT.employees = YT.employees.astype(int)
+print("fhidf")
+print(hist1)
 
-print(YT.assets)
-print(YT.profits)
+ans1 = distribution.findAnomalies(hist1,hist2,hist3,0.1)
+ans2 = distribution.findAnomalies(hist1,hist2,hist4,0.1)
 
-print(YT.assets)
-
-#print(Xp)
-#print(Yp)
-#AnomalyDetection.plotData(Yp)
-
-# plot the results
-
-print("Choose Variable to do Anomaly Detection")
-print("1. assets")
-print("2. profits")
-print("3. pd")
-print("4. liabilities")
-print("5. assets short term")
-print("6. assets long term")
-print("7. equity")
-print("8. sales")
-print("9. employees")
-
-choice = input("Choice?")
-
-if choice == 1:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 1:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 1:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 1:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 2:
-    AnomalyDetection.plot_results(Xp, YT.profits, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Profits")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.profits, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 3:
-    AnomalyDetection.plot_results(Xp, YT.pd, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Pd")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.pd, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 4:
-    AnomalyDetection.plot_results(Xp, YT.liabilities, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Liabilities")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.liabilities, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 5:
-    AnomalyDetection.plot_results(Xp, YT.assets_short_term, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Short Term Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets_short_term, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 6:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
-
-if choice == 1:
-    AnomalyDetection.plot_results(Xp, YT.assets, window_size=100, text_xlabel="Index", sigma_value=3,
-                 text_ylabel="Assets")
-    events = AnomalyDetection.anomalies_stationary_stddev(YT.assets, window_size=5, sigma=3)
-
-    # Display the anomaly dict
-    print("Information about the anomalies model:{}".format(events))
+print(ans1)
+print(ans2)
